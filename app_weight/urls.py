91ae475ac1bd6b3302a1e3915bb573f358c07048
YankielG <decybelq@o2.pl>
2024-08-11 12:18:11 +0200

    dodanie dodatkowych podstron tymczasowo w app waga - potem do zmiany

--- app_weight/urls.py
+++ app_weight/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import (views)
 
 urlpatterns = [
-    path('', views.all_weights, name='all_weights_url')
+    path('', views.all_weights, name='all_weights_url'),
+    path('info/', views.info, name='info_url'),
+    path('error/', views.error, name='error_url'),
+    path('home/', views.home, name='home_url')
+
 ]
--- app_weight/views.py
+++ app_weight/views.py
@@ -8,3 +8,14 @@ def all_weights(request):
         'weights': found_weights,
     }
     return render(request,'app_weight/all_weights.html', context)
+
+def info(request):
+    return  render(request,'app_weight/info.html')
+
+
+def error(request):
+    return  render(request,'app_weight/error.html')
+
+
+def home(request):
+    return  render(request,'app_weight/home.html')
