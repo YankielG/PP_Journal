c41dcfb2a0dc79aa3413c74e382c6808011c9d9e
YankielG <decybelq@o2.pl>
2024-09-13 06:26:28 +0200

    forms attr valid

--- app_pressure/forms.py
+++ app_pressure/forms.py
@@ -1,11 +1,22 @@
 from django import forms
 from django.core.validators import MinValueValidator, RegexValidator, MaxValueValidator, MinLengthValidator
 from django.forms.utils import ErrorList
+from django.core.exceptions import ValidationError
 from . import models
+from datetime import datetime, timedelta
+
+def validate_date(value):
+    if value.date() < datetime.now().date() - timedelta(weeks=10):
+        raise ValidationError(f'{value} nie jest datą max 1 rok wstecz')
 
 class Add_pressure_Form(forms.Form):
-    shrink = forms.DecimalField(label='Ciśnienie skurczowe', max_digits=5, decimal_places=1)
-    diastole = forms.DecimalField(label='Ciśnienie rozkurczowe', max_digits=5, decimal_places=1)
-    pulse = forms.DecimalField(label='Tętno', max_digits=5, decimal_places=1)
-    date = forms.DateTimeField(label='Data'),
-    comments = forms.CharField(label='Uwagi', max_length=255)
+    shrink = forms.DecimalField(label='Ciśnienie skurczowe', max_digits=5, decimal_places=1, validators=[MinValueValidator(50), MaxValueValidator(200)])
+    diastole = forms.DecimalField(label='Ciśnienie rozkurczowe', max_digits=5, decimal_places=1, validators=[MinValueValidator(50), MaxValueValidator(150)])
+    pulse = forms.DecimalField(label='Tętno', max_digits=5, decimal_places=1, validators=[MinValueValidator(40), MaxValueValidator(150)])
+    date = forms.DateTimeField(label='Data', validators=[validate_date], widget=forms.DateTimeInput(attrs={'class': 'datepicker', 'type': datetime}), initial=datetime.now().strftime("%Y-%m-%dT%H:%M")),
+    comments = forms.CharField(label='Uwagi', max_length=255, validators=[MinLengthValidator(2)], widget=forms.Textarea(attrs={'class': 'form-control', 'maxlength': 150, 'rows': 5, })),
+
+def __init__(self, *args, **kwargs):
+    super(Add_pressure_Form, self).__init__(*args, **kwargs)
+    for visible_field in self.visible_fields():
+        visible_field.field.widget.attrs['class'] = 'uk-input uk-margin-small-bottom'
--- app_pulse/forms.py
+++ app_pulse/forms.py
@@ -1,9 +1,20 @@
 from django import forms
 from django.core.validators import MinValueValidator, RegexValidator, MaxValueValidator, MinLengthValidator
 from django.forms.utils import ErrorList
+from django.core.exceptions import ValidationError
 from . import models
+from datetime import datetime, timedelta
+
+def validate_date(value):
+    if value.date() < datetime.now().date() - timedelta(weeks=10):
+        raise ValidationError(f'{value} nie jest datą max 1 rok wstecz')
 
 class Add_pulse_Form(forms.Form):
-    pulse = forms.DecimalField(label='Tętno', max_digits=5, decimal_places=1)
-    date = forms.DateTimeField(label='Data'),
-    comments = forms.CharField(label='Uwagi', max_length=255)
+    pulse = forms.DecimalField(label='Tętno', max_digits=5, decimal_places=1, validators=[MinValueValidator(50), MaxValueValidator(220)])
+    date = forms.DateTimeField(label='Data', validators=[validate_date], widget=forms.DateTimeInput(attrs={'class': 'datepicker', 'type': datetime}), initial=datetime.now().strftime("%Y-%m-%dT%H:%M")),
+    comments = forms.CharField(label='Uwagi', max_length=255, validators=[MinLengthValidator(2)], widget=forms.Textarea(attrs={'class': 'form-control', 'maxlength': 150, 'rows': 5, })),
+
+def __init__(self, *args, **kwargs):
+    super(Add_pulse_Form, self).__init__(*args, **kwargs)
+    for visible_field in self.visible_fields():
+        visible_field.field.widget.attrs['class'] = 'uk-input uk-margin-small-bottom'
