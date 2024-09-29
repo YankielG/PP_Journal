c9b1ff36c657294d8228904a60ec04ed19afbe3e
YankielG <decybelq@o2.pl>
2024-09-29 23:06:30 +0200

    proba edycji profilu

--- app_users/urls.py
+++ app_users/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('register', views.register, name='register_url'),
     path('password_reset', views.password_reset, name='password_reset_url'),
     path('password_change', views.password_change, name='password_change_url'),
+    path('profile_details', views.profile_details, name='profile_details_url'),
     path('edit_profile', views.edit_profile, name='edit_profile_url'),
 ]
--- app_users/views.py
+++ app_users/views.py
@@ -10,6 +10,8 @@ from django.core.paginator import Paginator
 
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required, user_passes_test
+from sqlalchemy.testing.suite.test_reflection import users
+
 from .forms import RegisterForm
 
 def register(request):
@@ -47,15 +49,41 @@ def password_change(request):
 
 @login_required
 def edit_profile(request):
-    # logged_user = request.user
-    # # found_user = user_passes_test(lambda user: user.first_name == logged_user.first_name)(request)
-    # if request.method == 'POST':
-    #     pass
+    logged_user = request.user
+    if request.method  == 'POST':
+        register_form = RegisterForm(request.POST, instance=logged_user)
+        logged_user.save()
+        if register_form.is_valid():
+            user_data = register_form
+            print(user_data)
+            user = register_form.save(commit=False)
+            user.id = user_data['id']
+            user.first_name = user_data['first_name']
+            user.last_name = user_data['last_name']
+            user.email = user_data['email']
+            user.save()
+            return redirect('profile_details_url')
 
     context = {
-        # 'user': found_user,
-        'time_value': datetime.now().strftime("%Y-%m-%dT%H:%M"),
-        'time_max': datetime.now().strftime("%Y-%m-%dT%H:%M"),
-        'time_min': (datetime.now() - timedelta(weeks=52)).strftime("%Y-%m-%dT%H:%M")
+        'user': logged_user,
+        'gender': 'man',
+        'time_value': datetime.now().strftime("%Y-%m-%d"),
+        'time_max': datetime.now().strftime("%Y-%m-%d"),
+        'time_min': (datetime.now() - timedelta(weeks=5200)).strftime("%Y-%m-%d"),
+        'register_form': RegisterForm(instance=logged_user)
     }
     return render(request, 'edit_profile.html', context)
+
+@login_required
+def profile_details(request):
+    logged_user = request.user
+    gender = "Mężczyzna"
+    context = {
+        'user': logged_user,
+        'gender': gender,
+        'time_value': datetime.now().strftime("%Y-%m-%d"),
+        'time_max': datetime.now().strftime("%Y-%m-%d"),
+        'time_min': (datetime.now() - timedelta(weeks=5200)).strftime("%Y-%m-%d"),
+        'register_form': RegisterForm()
+    }
+    return render(request, 'profile_details.html', context)
