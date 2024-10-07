016798954a5bba4a6cbec4c7e8f4926f7ea990cd
YankielG <decybelq@o2.pl>
2024-10-07 22:46:05 +0200

    proba rozszerzenia pol uzytkownika

--- app_users/models.py
+++ app_users/models.py
@@ -1,22 +1,26 @@
-# from django.db import models
-# from django.core.validators import MinValueValidator, MaxValueValidator, MaxLengthValidator
-# from django.core.exceptions import ValidationError
-# from datetime import datetime, timedelta
-# from django.contrib.auth.models import User
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator, MaxLengthValidator
+from django.core.exceptions import ValidationError
+from datetime import datetime, timedelta
+from django.contrib.auth.models import User
 #
 #
-# def validate_date(value):
-#     if value.date() < datetime.now().date() - timedelta(weeks=10):
-#         raise ValidationError(f'{value} nie jest datą max 1 rok wstecz')
-#
-#
-# # Create your models here.
-# class Users(models.Model):
-#     user = models.CharField(max_length=20)
-#     creation_date = models.DateTimeField(auto_now_add=True, validators=[validate_date])
-#     update_date = models.DateTimeField(auto_now=True, validators=[validate_date])
-#     comments = models.CharField(max_length=255, validators=[MaxLengthValidator(150)])
-#     owner = models.ForeignKey(User, on_delete=models.CASCADE)
-#
-#     def __str__(self):
-#         return f'Użyt.: {self.user} Data wpisu: {self.creation_date} Data akt.: {self.update_date} Uwagi: {self.comments} właściciel: {self.owner}'
+def validate_date(value):
+    if value.date() < datetime.now().date() - timedelta(weeks=5200):
+        raise ValidationError(f'{value} nie jest datą max 1 rok wstecz')
+
+
+class LoginHistory(models.Model):
+    login_date = models.DateTimeField(auto_now=True, validators=[validate_date])
+
+
+class UsersProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    update_date = models.DateTimeField(auto_now=True, validators=[validate_date])
+    birthday = models.DateField(validators=[validate_date])
+    gender = models.CharField(max_length=1, choices=(('M', 'Male'), ('F', 'Female')), default='M')
+    history = models.ForeignKey(LoginHistory, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return f'Użyt.: {self.user} Urodz.: {self.birthday} płec: {self.gender} akt.: {self.update_date} historia: {self.history}'
