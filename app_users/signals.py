fef213aeb6efb9c92f81ff12fa27f675bed4dbea
YankielG <decybelq@o2.pl>
2024-10-14 23:44:25 +0200

    dodanie do histori przegladarki

--- app_users/signals.py
+++ app_users/signals.py
@@ -1,7 +1,9 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.contrib.auth.signals import user_logged_in
+from django.contrib.auth.signals import user_logged_in, user_login_failed
 from django.contrib.auth.models import User
+from user_agents import parse
+
 from .models import UserProfile, LoginHistory
 
 
@@ -16,4 +18,14 @@ def save_user_profile(sender, instance, **kwargs):
 
 @receiver(user_logged_in)
 def log_user_login(sender, request, user, **kwargs):
-    LoginHistory.objects.create(user=user)
+    user_agent_string = request.META.get('HTTP_USER_AGENT', '')
+    user_agent = parse(user_agent_string)
+    browser_info = f"{user_agent.browser.family} - {user_agent.browser.version_string}"
+
+    forwarded = request.META.get('HTTP_X_FORWARDED_FOR')
+    if forwarded:
+        ip = forwarded.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+
+    LoginHistory.objects.create(user=user, user_agent=browser_info, ip_address=ip)
